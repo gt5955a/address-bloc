7477cef202790b7213c9c7d29e852e5b8ba5aa31
Garry <garry.tamhuang@gmail.com>
2017-04-11 22:11:18 -0400

    Assignment 5: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -16,7 +16,18 @@ require_relative 'entry'
       end
       index += 1
     end
-
     entries.insert(index, Entry.new(name, phone_number, email))
   end
+
+  def remove_entry(name, phone_number, email)
+    delete_entry = nil
+    entries.each do |entry|
+      if name == entry.name && phone == entry.phone_number && email == email_address
+        delete_entry = entry
+      end
+    end
+
+    entries.delete(delete_entry)
+  end
+
 end
