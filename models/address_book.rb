41caf34d5dd85f0d6c07d948804732905dafe8bb
Garry <garry.tamhuang@gmail.com>
2017-04-15 16:47:31 -0400

    Checkpoint 6: Completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,33 +1,31 @@
-require_relative 'entry'
+require_relative 'entry.rb'
 
-  class AddressBook
-    attr_reader :entries
+class AddressBook
+  attr_accessor :entries
 
-    def initialize
-      @entries = []
-    end
+  def initialize
+    @entries = []
   end
 
-  def add_entry(name, phone_number, email)
+  def remove_entry(name, phone, email)
+    delete_entry = nil
+    @entries.each do |entry|
+      if name == entry.name && phone == entry.phone_number && email == entry.email
+        delete_entry = entry
+      end
+    end
+
+    @entries.delete(delete_entry)
+  end
+
+  def add_entry(name, phone, email)
     index = 0
-    entries.each do |entry|
+    @entries.each do |entry|
       if name < entry.name
         break
       end
       index += 1
     end
-    entries.insert(index, Entry.new(name, phone_number, email))
+    @entries.insert(index, Entry.new(name, phone, email))
   end
-
-  def remove_entry(name, phone_number, email)
-    delete_entry = nil
-    entries.each do |entry|
-      if name == entry.name && phone == entry.phone_number && email == email_address
-        delete_entry = entry
-      end
-    end
-
-    entries.delete(delete_entry)
-  end
-
 end
